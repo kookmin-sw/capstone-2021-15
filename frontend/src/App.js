ddd820a8475afe187ac9e52175997c296b6e14d6
parkyolo <20181621@kookmin.ac.kr>
2021-02-25 20:15:44 +0900

    Navigation, Filter, Footer Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import CardComponent from "./components/CardComponent";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 import React from "react";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Navigation />
       <CardComponent PName="제품명" PColor="퍼스널컬러 톤" Categories="카테고리" Brand="브랜드"></CardComponent>
+      <Footer />
     </div>
   );
 }
