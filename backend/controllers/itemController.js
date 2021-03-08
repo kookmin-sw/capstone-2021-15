472deae74f4a6d0060e883058480bdd5ac3a6b9c
minjeongnew <20171623sw@kookmin.ac.kr>
2021-03-08 17:02:39 +0900

    feat: Modify item controller
    Signed-off-by: minjeongnew <20171623sw@kookmin.ac.kr>

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -20,10 +20,8 @@ module.exports = {
             return res.json({item, success: true});
         })
     },
-    // 퍼스널 컬러로 찾기 - 모든 상품 
-    // 이 경우 클릭 로그 순위로 정렬해서 아이템을 가져오지 않을까
     read_pcrs : async(req, res) => {
-        await Item.find({personalColor: req.params.personalColor}).sort({'click_log': -1}).exec(function(err, items){
+        await Item.find({personalColor: req.params.personalColor}, function(err, items){
             if(err) return res.status(500).json({ success: false, err });
             else{
                 if (items.length === 0) return res.send({message: "no items"}) // 아예 아이템이 존재하지 않을 때. (!items) 는 빈 배열[] 을 리턴함
@@ -32,9 +30,8 @@ module.exports = {
         })
     },
     // 퍼스널 컬러로 찾기 - 하나의 상품 
-    // 이 경우 클릭 로그 순위로 정렬해서 제일 순위가 높은 아이템을 가져오지 않을까
     // read_pcrs_one : async(req, res) => {
-    //     await Item.findOne({personalColor: req.params.personalColor}).sort({"click_log": -1}).exec(function(err, item){
+    //     await Item.findOne({personalColor: req.params.personalColor}, function(err, item){
     //         if(err) return res.status(500).json({ success: false, err });
     //         if(!item) return res.status(404).json({ success: false, message: 'item not found'});
     //         return res.json({item, success: true});
@@ -45,7 +42,7 @@ module.exports = {
     // category 2: 화장품/미용
     // category 3: 색조메이크업 이런 식인듯
     read_category2 : async(req, res) => {
-        await Item.find({category2: req.params.category2}).sort({'click_log': -1}).exec(function(err, items){
+        await Item.find({category2: req.params.category2}, function(err, items){
             if(err) return res.status(500).json({ success: false, err });
             else{
                 if (items.length === 0) return res.send({message: "no items"}) // 아예 아이템이 존재하지 않을 때. (!items) 는 빈 배열[] 을 리턴함
