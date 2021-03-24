4665c96444039c4c6dca89d1e8535cafcd48e534
minjeongnew <20171623sw@kookmin.ac.kr>
2021-03-24 13:41:11 +0900

    feat: Modify item models and controller
    Signed-off-by: minjeongnew <20171623sw@kookmin.ac.kr>

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -37,10 +37,7 @@ module.exports = {
     //         return res.json({item, success: true});
     //     })
     // },
-    // 카테고리로 찾기 - 모든 상품
-    // 네이버 api 에서 제공하는 아이템의 카테고리는 4개임 -> 데이터 가공을 먼저 해야함
-    // category 2: 화장품/미용
-    // category 3: 색조메이크업 이런 식인듯
+
     read_category2 : async(req, res) => {
         await Item.find({category2: req.params.category2}, function(err, items){
             if(err) return res.status(500).json({ success: false, err });
@@ -50,22 +47,5 @@ module.exports = {
             }
         })
     },
-    // 카테고리로 찾기 - 하나의 상품
-    // read_category2_one : async(req, res) => {
-    //     await Item.findOne({category2: req.params.category2}, function(err, item){
-    //         if(err) return res.status(500).json({ success: false, err });
-    //         if(!item) return res.status(404).json({ success: false, message: 'item not found'});
-    //         return res.json({item, success: true});
-    //     })
-    // },
 
-    read_category3 : async(req, res) => {
-        await Item.find({category3: req.params.category3}, function(err, item){
-            if(err) return res.status(500).json({ success: false, err });
-            else{
-                if (items.length === 0) return res.send({message: "no items"}) // 아예 아이템이 존재하지 않을 때. (!items) 는 빈 배열[] 을 리턴함
-                return res.json({items, success: true});
-            }
-        })
-    },
 }
