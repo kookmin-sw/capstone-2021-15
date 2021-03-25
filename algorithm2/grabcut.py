83bd11b0a48d63cbd506b2a9fd5ba9d3859b4c9a
manchann <chl8273@naver.com>
2021-03-26 01:00:07 +0900

    update grabcut.py

--- algorithm2/grabcut.py
+++ algorithm2/grabcut.py
@@ -8,14 +8,14 @@ bgdModel = np.zeros((1, 65), np.float64)
 fgdModel = np.zeros((1, 65), np.float64)
 # Step 1
 
-rect = (120, 10, 100, 100)  # (x, y, width, height)
+rect = (120, 10, 100, 80)  # (x, y, width, height)
 cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 1, cv2.GC_INIT_WITH_RECT)
 
-# Step 2
-newmask = cv2.imread('./test.jpg', 0)
-mask[newmask == 0] = 0
-mask[newmask == 255] = 1
-cv2.grabCut(img, mask, None, bgdModel, fgdModel, 1, cv2.GC_INIT_WITH_MASK)
+# # Step 2
+# newmask = cv2.imread('./test.jpg', 0)
+# mask[newmask == 0] = 0
+# mask[newmask == 255] = 1
+# cv2.grabCut(img, mask, None, bgdModel, fgdModel, 1, cv2.GC_INIT_WITH_MASK)
 mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
 img = img * mask2[:, :, np.newaxis]
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
