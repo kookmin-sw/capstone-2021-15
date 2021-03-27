6ab160251049f0125b4c9e1d4d5b5c6a1a60020a
minjeongnew <20171623sw@kookmin.ac.kr>
2021-03-27 13:27:09 +0900

    feat: Add frontend to merge backend but not completed
    Signed-off-by: minjeongnew <20171623sw@kookmin.ac.kr>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import React from "react";
 import Carousel from "./components/CardCarousel";
 import Recommand from "./components/Recommand";
-import Main from "./Main";
+import Auth from './hoc/auth'
+import MainPage from "./views/MainPage/MainPage"
 import Category from "./Category";
+import LoginPage from './views/LoginPage/LoginPage'
+import SignUpPage from './views/SignUpPage/SignUpPage'
 import {Link, Route, BrowserRouter as Router} from "react-router-dom"
 
 function App() {
@@ -14,13 +17,13 @@ function App() {
     return (
         <Router>
             <div className="App">
-                <Route exact path="/main" component={Main}/>
+                <Route exact path="/main" component={Auth(MainPage, true)}/>
                 {/*<Route path="/product" component={Product}/>*/}
                 {/*<Route path="/mypage" component={Mypage}/>*/}
                 <Route path="/category" component={Category}/>
                 {/*<Route path="/" component={Welcome}/>*/}
-                {/*<Route path="/login" component={Login}/>*/}
-                {/*<Route path="/signup" component={Signup}/>*/}
+                <Route path="/login" component={Auth(LoginPage, false)}/>
+                <Route path="/signup" component={Auth(SignUpPage, false)}/>
             </div>
         </Router>
     );
