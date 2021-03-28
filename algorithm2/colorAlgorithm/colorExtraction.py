5fd456cebd0f658474c022970499223596b38969
manchann <chl8273@naver.com>
2021-03-28 16:56:59 +0900

    get https url

--- algorithm2/colorAlgorithm/colorExtraction.py
+++ algorithm2/colorAlgorithm/colorExtraction.py
@@ -1,8 +1,11 @@
 import cv2
 from sklearn.cluster import KMeans
 import colorsys
-import urllib
+import urllib.request
 import numpy as np
+import ssl
+
+context = ssl._create_unverified_context()
 
 
 class DominantColors:
@@ -17,7 +20,7 @@ class DominantColors:
 
     def dominantColors(self):
         # read image
-        img = cv2.imread(self.IMAGE)
+        img = self.IMAGE
 
         # convert to rgb from bgr
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -41,14 +44,16 @@ class DominantColors:
         # returning after converting to integer from float
         return self.COLORS.astype(int)
 
+
 def url_to_image(url):
-	# download the image, convert it to a NumPy array, and then read
-	# it into OpenCV format
-	resp = urllib.urlopen(url)
-	image = np.asarray(bytearray(resp.read()), dtype="uint8")
-	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-	# return the image
-	return image
+    # download the image, convert it to a NumPy array, and then read
+    # it into OpenCV format
+    resp = urllib.request.urlopen(url, context=context)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    # return the image
+    return image
+
 
 img = url_to_image('https://cdn.chicor.com/images/product/20200813155626047.jpg')
 
@@ -59,6 +64,6 @@ print(colors)
 r = colors[0][0]
 g = colors[0][1]
 b = colors[0][2]
-hsv = colorsys.rgb_to_hsv(r/255, g/255, b/255)
-print(type(hsv))
+hsv = colorsys.rgb_to_hsv(r / 255, g / 255, b / 255)
 
+print(hsv)
