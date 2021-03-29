715aa66d47676a7daed987bdf8bf61ead163f643
parkyolo <20181621@kookmin.ac.kr>
2021-03-30 03:07:12 +0900

    modify footer, navigation, add product page router

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -13,25 +13,25 @@ const Navigation = ( props ) =>  {
 
     return (
         <>
-            <Nav className="navigation_inner justify-content-center">
+            <Nav className="navigation_inner">
                 <Nav  className="m-2 ">
                     <Nav.Link href="">HAIR</Nav.Link>
-                    {/* <Nav.Link href="">BASE</Nav.Link> */}
+                    <Nav.Link href="">BASE</Nav.Link>
+                    <Nav.Link href="">LENS</Nav.Link>
                     <Nav.Link href="">LIP</Nav.Link>
-                    <Nav.Link href="">EYE</Nav.Link>
-                    <Nav.Link href="">CHEEK</Nav.Link>
-                    {/* <Nav.Link href="">TOP</Nav.Link> */}
-                    <Button onClick={togglefilter}>
+                    <Nav.Link href="">TOP</Nav.Link>
+                    <Nav.Link href="">ACC</Nav.Link>
+                    {/* <Button onClick={togglefilter}>
                         <img className="filter_img" src={filterimg}></img>
                         tone
-                    </Button>
+                    </Button> */}
                 </Nav>
             </Nav>
-            <Row className="row justify-content-center bg-transparent">
+            {/* <Row className="row justify-content-center">
                 <Col className="col-sm-6">
                     <Filter togglefilter={togglefilter} filter={filter}></Filter>
                 </Col>
-            </Row>
+            </Row> */}
         </>
     )
 }
