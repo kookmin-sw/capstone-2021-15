1270161625cd325cb78bfd2336f69ce75b9f73ea
minjeongnew <20171623sw@kookmin.ac.kr>
2021-04-18 02:12:13 +0900

    feat: Add redux
    - /src/_actions
    - /src/_reducers
    - /src/index.js
    
    Signed-off-by: minjeongnew <20171623sw@kookmin.ac.kr>

---
+++ frontend/src/_actions/user_actions.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import {
+    LOGIN_USER,
+    AUTH_USER,
+    SIGNUP_USER,
+    LOGOUT_USER
+} from './types'
+
+export function signupUser(dataToSubmit) {
+
+}
+export function loginUser(dataToSubmit) {
+
+}
+export function auth() {
+    
+}
+export function logoutUser(){
+
+}
