f60d18f52cbafa07a2c65e300e7bcb69f5e3bb61
Subin <39542978+subeans@users.noreply.github.com>
2021-05-23 02:58:55 +0900

    using feedback results and update scores

--- recommendation_method/gru4rec/our_gru4rec.py
+++ recommendation_method/gru4rec/our_gru4rec.py
@@ -2,16 +2,22 @@ import argparse
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+
 import similarity_buydata as bdsim
+import feedback as fb
 import random
 import sys 
 
 import keras
 import keras.backend as K
 from keras.models import Model
-from keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint
+
+from keras.utils import to_categorical
 from keras.losses import categorical_crossentropy
+from keras.losses import binary_crossentropy
+from keras.losses import sparse_categorical_crossentropy
+
 from keras.layers import Input, Dense, Dropout, GRU
 
 class SessionDataset:
@@ -148,9 +154,12 @@ class SessionDataLoader:
                 end[idx] = click_offsets[session_idx_arr[maxiter] + 1]
 
 # def create_model(batch_size,train_n_items):   
-def create_model(args):   
+def create_model(args):  
+    ##### Loss 변경 부분 ##### 
+    loss = args.loss
+    ##### Units #####
+    hidden_units = args.units
     emb_size = 50
-    hidden_units = 1000
     size = emb_size
 
     inputs = Input(batch_shape=(args.batchsize, 1, args.train_n_items))
@@ -160,7 +169,7 @@ def create_model(args):
     predictions = Dense(args.train_n_items, activation='softmax')(drop2)
     model = Model(inputs=inputs, outputs=[predictions])
     opt = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-    model.compile(loss=categorical_crossentropy, optimizer=opt)
+    model.compile(loss=loss, optimizer=opt)
     model.summary()
 
     filepath='./model_checkpoint.h5'
@@ -169,10 +178,11 @@ def create_model(args):
     return model
 
 # def train_model(model, test_data, train_data,batchsize,epochs, train_samples_qty,eval_all_epochs):
-def train_model(model, args,item_sim,k=10):
+def train_model(model, args,item_sim):
     train_dataset = SessionDataset(args.train_data)
     model_to_train = model
     batchsize = args.batchsize
+    k = args.k
 
     for epoch in range(1, args.epochs):
         with tqdm(total=args.train_samples_qty) as pbar:
@@ -199,20 +209,20 @@ def train_model(model, args,item_sim,k=10):
 
         if args.eval_all_epochs:
             recall, mrr, our_recall, our_mrr = get_metrics(model_to_train, args, train_dataset.itemmap,item_sim)
-            print("- Recall@{} epoch {}: {:5f}".format(k, epoch, recall))
-            print("- MRR@{}    epoch {}: {:5f}\n".format(k, epoch, mrr))
-            print("- our Recall@{} epoch {}: {:5f}".format(k, epoch, our_recall))
-            print("- our MRR@{}    epoch {}: {:5f}\n".format(k, epoch, our_mrr))
+            print("Recall@{} epoch {}: {:5f}".format(k, epoch, recall))
+            print("MRR@{}    epoch {}: {:5f}\n".format(k, epoch, mrr))
+            print("our Recall@{} epoch {}: {:5f}".format(k, epoch, our_recall))
+            print("our MRR@{}    epoch {}: {:5f}\n".format(k, epoch, our_mrr))
 
     if not args.eval_all_epochs:
         recall, mrr, our_recall,our_mrr = get_metrics(model_to_train, args, train_dataset.itemmap,item_sim)
-        print("- Recall@{} epoch {}: {:5f}".format(k, args.epochs, recall))
-        print("- MRR@{}    epoch {}: {:5f}\n".format(k, args.epochs, mrr))
-        print("- our Recall@{} epoch {}: {:5f}".format(k, epoch, our_recall))
-        print("- our MRR@{}    epoch {}: {:5f}\n".format(k, epoch, our_mrr))
+        print("Recall@{} epoch {}: {:5f}".format(k, args.epochs, recall))
+        print("MRR@{}    epoch {}: {:5f}\n".format(k, args.epochs, mrr))
+        print("our Recall@{} epoch {}: {:5f}".format(k, args.epochs, our_recall))
+        print("our MRR@{}    epoch {}: {:5f}\n".format(k, args.epochs, our_mrr))
 
 
-def get_top_item(id_idx , select_item_index ):     
+def convert_idx_to_id(id_idx , select_item_index ):     
     select_item_ids = []
     for item_idx in select_item_index :
         try:
@@ -220,34 +230,32 @@ def get_top_item(id_idx , select_item_index ):
             select_item_ids.append(select_item_id)
         except:
             print(item_idx)
-    # print(select_item_id)
     return select_item_ids
 
 def get_sim_item(id_idx,select_item_id,item_sim):
     # buy data 
     similar_item_idx = []
-    similar_item_id = []
     for one_id in select_item_id : 
         similarity_item_id = bdsim.pick_sim_items(one_id,item_sim)
-        similar_item_id.append(similar_item_id)
         try:
             sim_item_idx = id_idx[id_idx['ItemId']==similarity_item_id].iloc[0]['item_idx']
             similar_item_idx.append(sim_item_idx)
         except:
             continue
-    return similar_item_idx ,similar_item_id
+    return similar_item_idx
 
-# def get_metrics(model, test_data, batchsize, train_n_items, train_generator_map, recall_k=3, mrr_k=3):
-def get_metrics(model, args,train_generator_map,item_sim,k=10):
+def get_metrics(model, args,train_generator_map,item_sim):
     train_dataset = SessionDataset(args.train_data)
     test_dataset = SessionDataset(args.test_data, itemmap=train_generator_map)
     test_generator = SessionDataLoader(test_dataset, batch_size=args.batchsize)
 
+    k = args.k
     n = 0
     rec_sum = 0
     mrr_sum = 0
     rec_our_sum = 0
     mrr_our_sum=0
+    better=0
         
     print("Evaluating model...")
     for feat, label, mask in test_generator:
@@ -265,29 +273,28 @@ def get_metrics(model, args,train_generator_map,item_sim,k=10):
         pred = model.predict(input_oh, batch_size=args.batchsize)
         
         for row_idx in range(feat.shape[0]):
-            # recall_k == 30개 
             pred_row = pred[row_idx]
             label_row = target_oh[row_idx]
 
             pred_top_k = pred_row.argsort()[-k:][::-1]
-            # [ 2091  6895 14688 16920 16957 14428  8895 10588 20127 14072]
             # print(pred_top_k)
+            # [ 2091  6895 14688 16920 16957 14428  8895 10588 20127 14072]
             select_item_index = pred_top_k.tolist()
 
-            select_item_id = get_top_item(train_generator_map, select_item_index)
-            similar_item_idx ,similar_item_id = get_sim_item(train_generator_map,select_item_id,item_sim)
+            select_item_id = convert_idx_to_id(train_generator_map, select_item_index)
+            similar_item_idx = get_sim_item(train_generator_map,select_item_id,item_sim)
 
-            # 10개 보다 많은 우리 our_idx
+            # gru 에서 뽑은 k item + similar items 
             our_idx = select_item_index + similar_item_idx
-            # print("our K :",len(our_idx))
             
-            # top + sim 에서 랜덤으로 k 개 추출 
+            # top + sim > k 이므로 랜덤으로 k 개 추출 
             our_idx_k = random.sample(our_idx,k)
             our_idx = np.asarray(our_idx_k)
 
             rec_idx =  pred_row.argsort()[-k:][::-1]
+            rec_idx = rec_idx.tolist()
             mrr_idx =  pred_row.argsort()[-k:][::-1]
-            
+            mrr_idx = mrr_idx.tolist()
             tru_idx = label_row.argsort()[-1:][::-1]
             n += 1
 
@@ -298,21 +305,32 @@ def get_metrics(model, args,train_generator_map,item_sim,k=10):
                 rec_our_sum +=1
                 
             if tru_idx[0] in our_idx:
-                mrr_our_sum += 1/int((np.where(our_idx == tru_idx[0])[0]+1))
-            
+                try :
+                    mrr_our_sum += 1/int((np.where(our_idx == tru_idx[0])[0]+1))
+                except :
+                    continue
             if tru_idx[0] in mrr_idx:
                 mrr_sum += 1/int((np.where(mrr_idx == tru_idx[0])[0]+1))
                 
-     
+            recall = rec_sum/n 
+            mrr = mrr_sum/n
+            our_recall = rec_our_sum/n
+            our_mrr = mrr_our_sum/n
+
+            # 순위를 고려하지 않는 추천은 recall 이 중요하다고 생각됨 
+            if recall <= our_recall :
+                better+=1
+                
+                ################# score ####################
+                # GRU4REC 만을 적용했을 때 보다 좋은 결과일 경우 score ++
+                # rw.reward_to_mongo(our_ids)
             
-    recall = rec_sum/n 
-    mrr = mrr_sum/n
-    our_recall = rec_our_sum/n
-    our_mrr = mrr_our_sum/n
     print("------------ours--------------")
     print("recall : ",our_recall ," & mrr : ",our_mrr)
     print("----------baseline-----------")
     print("recall : ",recall ," & mrr : ",mrr)
+    print("OURS BETTER or SAME : ",better,"/",n,"counts")
+
 
     return recall, mrr, our_recall,our_mrr
     
@@ -320,16 +338,21 @@ def get_metrics(model, args,train_generator_map,item_sim,k=10):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Keras GRU4REC: session-based recommendations')
-    parser.add_argument('--resume', type=str, help='stored model path to continue training')
     parser.add_argument('--train-path', type=str, default='~/data/sample_dataset/rsc15_train_sample_trn.txt')
-    parser.add_argument('--eval-only', type=bool, default=False)
+    parser.add_argument('--eval-only', type=bool, default=True)
     parser.add_argument('--dev-path', type=str, default='~/data/sample_dataset/rsc15_train_sample_valid.txt')
     parser.add_argument('--test-path', type=str, default='~/data/sample_dataset/rsc15_test_sample.txt')
     parser.add_argument('--buy-path',type=str,default='~/data/sample_dataset/rsc15_buydata_preprocessed.txt')
+    
     parser.add_argument('--batchsize', type=str, default=512)
     parser.add_argument('--eval-all-epochs', type=bool, default=False)
     parser.add_argument('--save-weights', type=bool, default=False)
     parser.add_argument('--epochs', type=int, default=2)
+    parser.add_argument('--k',type=int,default=20)
+    parser.add_argument('--loss',type=str,default=categorical_crossentropy)
+    parser.add_argument('--units',type=int,default=1000)
+
+
     args = parser.parse_args()
 
     args.train_data = pd.read_csv(args.train_path, sep='\t', dtype={'ItemId': np.int64})
@@ -342,25 +365,20 @@ if __name__ == '__main__':
     args.train_samples_qty = len(args.train_data['SessionId'].unique()) + 1
     args.test_samples_qty = len(args.test_data['SessionId'].unique()) + 1
 
-    if args.resume:
-        try:
-            model = keras.models.load_model(args.resume)
-            print("Model checkpoint '{}' loaded!".format(args.resume))
-        except OSError:
-            print("Model checkpoint could not be loaded. Training from scratch...")
-            model = create_model(args)
-    else:
-        model = create_model(args)
-    
+    model = create_model(args)
+    # similarity 계산 
     item_sim = bdsim.settings(args.buy_data)
-    k=10
+    # feedback 적용하여 K ratio 조절하는 부분 
+    # ratio = fb.count_feedbacks()
+    k=args.k
 
     if args.eval_only:
         train_dataset = SessionDataset(args.train_data)
         recall, mrr, our_recall,our_mrr = get_metrics(model, args, train_dataset.itemmap,item_sim)
-        print("- Recall@{} epoch {}: {:5f}".format(k, -1, recall))
-        print("- MRR@{}    epoch {}: {:5f}\n".format(k, -1, mrr))
-        print("- our Recall@{} epoch {}: {:5f}".format(k, -1, our_recall))
-        print("- our MRR@{}    epoch {}: {:5f}\n".format(k, -1, our_mrr))
+        print("GRU UNIT :",args.units ,"Loss : ",args.loss)
+        print("Recall@{}  : {:5f}".format(k,  recall))
+        print("MRR@{}    : {:5f}\n".format(k,  mrr))
+        print("our Recall@{}  : {:5f}".format(k, our_recall))
+        print("our MRR@{}     : {:5f}\n".format(k,  our_mrr))
     else:
         train_model(model, args,item_sim)
